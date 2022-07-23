9a37661c3730726882f5964824eba375a3542e33
jvale <jvale@magnasistemas.com.br>
2022-07-23 12:55:50 -0300

    Codigo refatorado

--- main.js
+++ main.js
@@ -18,63 +18,76 @@ function getAPIArticle(params){
         .then(res => res.json())
         .then(
             (resultado)=>{
-                console.log(resultado.results);
                 createArticle(resultado.results);         
             });
 }
 
-function createCarousel(element){
-    let titulo = document.querySelector(".titulo-principal");
-    let descricao = document.querySelector(".descricao-principal");
+function validaRetornoAPI(element, indice=0){
+    let i = indice;
+    while (element[i].multimedia == false){
+        i+=1;
+    }
+    return i;
+}
 
-    if(element[1].multimedia[0].url){
-        titulo.innerHTML = element[1].title;
-        descricao.innerHTML= element[1].abstract;
-        car = document.querySelector('.imagem1');
-        car.src= element[1].multimedia[0].url
-    }
+function createCarousel(element){
+    let titulo = document.querySelector("#titulo__carrosel_1");
+    let descricao = document.querySelector(".descricao-principal");
+    car = document.querySelector('.imagem1');
+    linkImagem = document.querySelector('#link-imagem1');
+    let indice= validaRetornoAPI(element);
+   
+   //imagem 1
+    titulo.innerHTML = element[indice].title;
+    descricao.innerHTML= element[indice].abstract;
+    car.src= element[indice].multimedia[0].url;
+    linkImagem.href = element[indice].url
+    indice= validaRetornoAPI(element, indice=+1);
+
     
-    if(element[2].multimedia[0].url){
-        titulo= document.querySelector('.titulo-secundario')
-        descricao= document.querySelector('.descricao-secundario')
-        titulo.innerHTML = element[2].title
-        descricao.innerHTML= element[2].abstract
-        car2 = document.querySelector('.imagem2');
-        car2.src= element[2].multimedia[0].url
-    }
+    //imagem 2
+    titulo= document.querySelector('#titulo__carrosel_2')
+    descricao= document.querySelector('.descricao-secundario')
+    linkImagem = document.querySelector('#link-imagem2');
+    titulo.innerHTML = element[indice].title
+    descricao.innerHTML= element[indice].abstract
+    car2 = document.querySelector('.imagem2');
+    car2.src= element[indice].multimedia[0].url
+    linkImagem.href = element[indice].url
 }
 
 function createArticle(element){
     element.forEach(element => {
-        let cont = 0;
-        let divConteudo = document.querySelector('.conteudo');
-        
-        let divCard = document.createElement('div');
-        let cardBody = document.createElement('div');
-        let img = document.createElement('img');
-        let tituloCard = document.createElement('h4');
-        let descricao = document.createElement('p');
-        let link = document.createElement('a');
+        if (element.abstract){
+            let divConteudo = document.querySelector('.conteudo');
+            
+            let divCard = document.createElement('div');
+            let cardBody = document.createElement('div');
+            let img = document.createElement('img');
+            let tituloCard = document.createElement('h4');
+            let descricao = document.createElement('p');
+            let link = document.createElement('a');
 
-        divCard.classList.add('card', 'd-flex', 'w-25');
-        cardBody.classList.add('card-body');
-        img.classList.add('card-img-top', 'imagem-card', 'p-1');
-        tituloCard.classList.add('card-title')
-        descricao.classList.add('card-text');
-        
-        img.src=element.multimedia[cont].url;
-        tituloCard.innerHTML = element.title;
-        descricao.innerHTML= element.abstract;
-        link.href= element.url;
-        
-        cardBody.append(tituloCard);
-        cardBody.append(descricao);
-        link.append(img);
+            divCard.classList.add('card', 'd-flex');
+            cardBody.classList.add('card-body');
+            img.classList.add('card-img-top', 'imagem-card', 'p-1');
+            tituloCard.classList.add('card-title')
+            descricao.classList.add('card-text');
+            
+            img.src=element.multimedia[0].url;
+            tituloCard.innerHTML = element.title;
+            descricao.innerHTML= element.abstract;
+            link.href= element.url;
+            
+            cardBody.append(tituloCard);
+            cardBody.append(descricao);
+            link.append(img);
 
-        divCard.append(link);
-        divCard.append(cardBody);
+            divCard.append(link);
+            divCard.append(cardBody);
 
-        divConteudo.append(divCard);
+            divConteudo.append(divCard);
+        }
     });
 }
 
